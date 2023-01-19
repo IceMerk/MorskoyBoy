a8b599361eba7cb693d6c0eebb1036daf27ed7dd
Alexander <nestrom2007@yandex.ru>
2023-01-20 02:55:58 +0300

    add class Player add metod's Board

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ class Dot:  # Класс точек
 
 class Ship:  # Класс кораблей
     def __init__(self, na4alo, dlina, orentacia):
-        self.na4alo = na4alo # Начальная точка корабля
-        self.dlina = dlina # Длина корабля
-        self.orentacia = orentacia # горизонтально или вертикально
-        self.hp = dlina # Жизни корабля равные его длине
+        self.na4alo = na4alo  # Начальная точка корабля
+        self.dlina = dlina  # Длина корабля
+        self.orentacia = orentacia  # горизонтально или вертикально
+        self.hp = dlina  # Жизни корабля равные его длине
 
     @property  # Декоратор, определяет свойства, можем вызывать функцию без call
     def dots(self):
@@ -42,45 +42,118 @@ class Ship:  # Класс кораблей
             other_y = self.na4alo.y
 
             if self.orentacia == 0:  # Смотрим как расположен корабль
-                other_x += 1
-            else:
-                other_y += 1
+                other_x += i
+            elif self.orentacia == 1:
+                other_y += i
 
             ship_dots.append(Dot(other_x, other_y))
 
         return ship_dots  # Возвращаем корабль
 
-    def popal(self, shot): # Проверка на попадание в корабль
+    def popal(self, shot):  # Проверка на попадание в корабль
         return shot in self.dots
+
 
 class Board:
     def __init__(self, size=6, hid=False):
-        self.size = size # Размер поля
-        self.hid = hid # Нужно ли скрывать тип bool
-        self.field = [['О']*6 for i in range(self.size)] # Поле
-        self.ships = [] # Спискок кораблей
-        self.busy_ships = [] # Список выстрелов мимо/по кораблям
-        self.death_ships = 0 # Список убитых кораблей
+        self.size = size  # Размер поля
+        self.hid = hid  # Нужно ли скрывать тип bool
+        self.field = [['О'] * 6 for i in range(self.size)]  # Поле
+        self.ships = []  # Спискок кораблей
+        self.busy_ships = []  # Список выстрелов мимо/по кораблям
+        self.death_ships = 0  # Список убитых кораблей
 
-    def add_ship(self):
-        pass
+    def add_ship(self, ship): # Добавление корабля на доску
+        for dot in ship.dots: # В цикле проверяем точки: в поле ли они и нету ли её в занятых
+            if self.out(dot) or dot in self.busy_ships:
+                raise Exception('Сюда нельзя ставить') # Если есть, то кидаем Ошибку
+        for dot in ship.dots: # Если всё норм
+            self.field[dot.x][dot.y] = '■' # Заменяем О на точку корабля
+            self.busy_ships.append(dot) # Добавляем в список занятых точек кораблей
+        self.ships.append(ship) # Добавляем в список кораблей
+        self.contour(ship)  # рисуем контур корабля
 
-    def contour(self):
-        pass
+    def contour(self, ship, view=False):
+        radius = [
+            (1, -1), (1, 0), (1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (-1, -1), (-1, 0), (-1, 1)
+        ]
+        for dot in ship.dots: # Берем точки корабля
+            for dot_x, dot_y in radius: # Берем список radius
+                check = Dot(dot.x + dot_x, dot.y + dot_y) # Прогоняем корабль по radius
+                if not (self.out(check)) and check not in self.busy_ships: # Если не входит в диапазон и нету в
+                    # списке точек/кораблей
+                    if view: # Если корабль подбит, рисуем возле него точки
+                        self.field[check.x][check.y] = '.'
+                    self.busy_ships.append(check) # Добавляем в занятые
 
-    def __str__(self): # Отрисовка поля
-        v = ' | 1 | 2 | 3 | 4 | 5 | 6 |' # Поле с цифрами сверху
-        for i, k in enumerate(self.field): # Рисуем поле и добавляем цифры сбоку
-            v += f'\n{i + 1} | {"|".join(k)} |'
+    def __str__(self):  # Отрисовка поля
+        v = '  | 1 | 2 | 3 | 4 | 5 | 6 |'  # Поле с цифрами сверху
+        for i, k in enumerate(self.field):  # Рисуем поле и добавляем цифры сбоку
+            v += f'\n{i + 1} | {" | ".join(k)} | '
 
-        if self.hid: # Если нужно скрыть корабли hid = True
+        if self.hid:  # Если нужно скрыть корабли hid = True
             v = v.replace('■', 'О')
+        return v
 
-    def out(self):
-        pass
+    def out(self, dot): # Проверяем находится ли точка за пределами доски
+        return not all([0 <= dot.x <= self.size, 0 <= dot.y <= self.size])
 
-    def shot(self):
-        pass
+    def shot(self, dot): # Метод выстрела
+        if self.out(dot): # Если выстрел вне доски
+            raise BoardOutException()
+        if dot in self.busy_ships: # Если выстрел уже был
+            raise BoardUseException()
 
-s = Ship(Dot(1, 2), 4, 0)
-print(s.popal(Dot(2, 2)))
+        self.busy_ships.append(dot) # Если всё ок, добавляем в список занятых
+
+        for ship in self.ships: # Ищем в списке кораблей
+            if dot in ship.popal(dot): # Сверяем - попали или нет
+                ship.hp -= 1 # Если попали, то уменьшаем хп у корабля
+                self.field[dot.x][dot.y] = 'X'
+                if ship.hp == 0: # Если хп не осталось
+                    self.death_ships += 1 # Пополняем счетчик мертвых
+                    self.contour(ship, view=True) # Рисуем вокруг него точки
+                    print('Корабль потоплен')
+                    return False
+                else:
+                    print('Корабль подбит')
+                    return True
+
+        self.field[dot.x][dot.y] = '.' # Если попали Мимо
+        print('Мимо')
+        return False
+
+    def start(self): # Обнуляем список занятого
+        self.busy_ships = []
+
+class Player:
+    def __init__(self, board, board_second):
+        self.board = board
+        self.board_second = board_second
+
+    def ask(self): # Долго гуглил это, пока не нашел исключение
+        raise NotImplementedError()
+
+    def move(self): # Спрашиваем игрока сделать выстрел
+        while True:
+            try:
+                t = self.ask() # Запрос выстрела
+                r = self.board_second.shot(t) # Проверка на доске противника
+                return r
+            except Exception as e:
+                print(e)
+
+class AI(Player):
+    pass
+
+class User(Player):
+    pass
+
+
+b = Board()
+
+b.add_ship(Ship(Dot(1, 2), 4, 0))
+b.add_ship(Ship(Dot(0, 0), 2, 1))
+print(b)
